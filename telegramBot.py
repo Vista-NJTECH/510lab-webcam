1ebc61fc0c1cee7fc0fbf2a1c67b93544a7d6b72
Cai Jianwen <2750417853@qq.com>
2023-09-26 11:10:58 +0800

    feat: Allow multi cams

--- telegramBot.py
+++ telegramBot.py
@@ -5,23 +5,38 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-bot = telebot.TeleBot("your_telegram_bot_token")
+bot = telebot.TeleBot("telebot_token")
 
 
-def take_photo():
-    response = requests.get('http://localhost:8084/?action=snapshot')
+def take_photo(cam):
+    host = "172.18.192.245"
+    if cam == 2:
+        host = "172.18.192.199"
+    path = "http://{}:8084/?action=snapshot".format(host)
+    response = requests.get(path)
     return Image.open(BytesIO(response.content))
 
 
-@bot.message_handler(commands=['snapshot'])
-def send_photo(message):
+def send_photo(msg, cam):
     print('[INFO] Sending snapshot...')
     try:
-        bot.send_photo(message.chat.id, take_photo())
-        print('[INFO] Snapshot sended!')
+        bot.send_photo(msg.chat.id, take_photo(cam))
+        print('[INFO] Snapshot sent!')
     except:
-        print('[ERROR] Failed to send snapshot!')
+        bot.send_message(msg.chat.id, "Snapshot failed!")
+        print('[ERROR] Snapshot failed!')
+
+
+@bot.message_handler(commands=['snapshot'])
+def send_photo_handler(msg):
+    send_photo(msg, 1)
+
+
+@bot.message_handler(commands=['snapshot2'])
+def send_photo_handler(msg):
+    send_photo(msg, 2)
 
 
 print('[INFO] Telegram bot listening...')
+
 bot.polling()
